da4c362bd3e7d7ae7cac7743dec95937f39f297e
Rob Wood <rob.a.wood@gmail.com>
2017-06-24 19:22:27 -0600

    Add query parameter for number of values

--- src/main/java/com/example/RestController.java
+++ src/main/java/com/example/RestController.java
@@ -1,5 +1,7 @@
 package com.example;
 
+import com.google.gson.Gson;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -16,16 +18,21 @@ public class RestController {
 
         get("/", (req, res) -> "SimpleDataService");
 
+        // {DataLink:URL:https://stackoverflow.com/questions/29127490/how-to-get-the-request-parameters-using-get-in-spark-java-framework}
         get("/values01", (req, res) -> {
             res.status(200);
             res.type("application/json");
+            Object check1 = req.queryParams();
+            Object check2 = req.queryParams("numValues");
+            int numValues = Integer.parseInt(req.queryParams("numValues"));
+            //int numValues = 10;
             long seed = 1;
             Random generator = new Random(seed);
-            List<Integer> values = new ArrayList<>(10);
-            for (int ix = 0; ix < 10; ix++) {
+            List<Integer> values = new ArrayList<>(numValues);
+            for (int ix = 0; ix < numValues; ix++) {
                 values.add(generator.nextInt(100));
             }
-            return values;
+            return new Gson().toJson(values);
         });
     }
 }
